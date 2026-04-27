2ac44f27aed1477d3c7c1776ab91c5fe04652cdb
Dipesh Luitel <dipeshluitel40@gmail.com>
2026-04-27 12:32:06 +0545

    Implement validation checks for crm_sales_details
    Added checks for invalid dates and data consistency in sales details.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -70,4 +70,58 @@ WHERE prd_end_dt < prd_start_dt
 SELECT * FROM silver.crm_prd_info
 
 
- 
+/*
+================================
+CHECKING 'silver.crm_sales_details'
+================================
+*/
+
+--Check For Invalid Dates
+--Expectation: No Result
+SELECT
+NULLIF(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0		--Checks For zero or Negative dates
+OR LEN(sls_order_dt) != 8	--Checks for incorrect date length
+OR sls_order_dt > 20261231	--Checks for Outlier
+OR sls_order_dt < 20021208	--Checks for Outlier
+  
+SELECT
+NULLIF(sls_ship_dt,0) sls_ship_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0		--Checks For zero or Negative dates
+OR LEN(sls_ship_dt) != 8	--Checks for incorrect date length
+OR sls_ship_dt > 20261231	--Checks for Outlier
+OR sls_ship_dt < 20021208	--Checks for Outlier
+
+SELECT
+NULLIF(sls_due_dt,0) sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_due_dt <= 0		--Checks For zero or Negative dates
+OR LEN(sls_due_dt) != 8	--Checks for incorrect date length
+OR sls_due_dt > 20261231	--Checks for Outlier
+OR sls_due_dt < 20021208	--Checks for Outlier
+
+-- The above tests can only be performed in the Bronze schema due to casting reasons
+
+--Check for invalid Date Orders
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_due_dt OR sls_order_dt > sls_ship_dt
+
+--Check Data Consistency: Between sales,quantity and Price
+-- Sales = Quantity * Price
+-- Values must not be Null, Zero or Negative
+
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity*sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <=0 OR sls_price <=0
+ORDER BY sls_sales, sls_quantity, sls_price
+
+-- Final Overview of Whole Table
+SELECT * FROM silver.crm_sales_details
